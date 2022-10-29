98dd4fd04f1b37f0aa31edd480ce91371ddb1847
RedGold31 <max10050031@gmail.com>
2022-10-29 11:17:09 +0700

    sas

--- massiv/main.c
+++ massiv/main.c
@@ -6,6 +6,7 @@ void print_mass(int a[]);
 int search_max(int a[]);
 int search_min(int a[]);
 int search_index(int a[], int size, int number);
+int reserve(int a[]);
 
 void set_mass(int a[])
 {
@@ -39,15 +40,15 @@ int search_max(int a[])
 int search_index(int a[], int size, int number)
 {
 
-    int number = -1;
+    number = 10;
 
-    for (int i = 0; i < size - 1; ++i) {
-        if (a[i] = number) {
-            number = i;
-            break;
-        }
+    for (int i = 0; i < number; ++i) {
+        scanf("%d\n", &a[i]);
+        printf("%d\n", a[i] - number);
+        break;
     }
-    return number
+
+    return number;
 }
 
 int search_min(int a[])
@@ -62,6 +63,14 @@ int search_min(int a[])
     return min;
 }
 
+int reserve(int a[])
+{
+    for (int i = 0; i < SIZE; ++i) {
+        printf(" %d ", a[SIZE - i - 1]);
+    }
+    printf("\n");
+}
+
 int main()
 {
 
@@ -69,10 +78,9 @@ int main()
     set_mass(a);
     print_mass(a);
     search_max(a);
+    int res = reserve(a);
     int max = search_max(a);
     int min = search_min(a);
-    int ind = search_index(a);
     printf("max - %d\n", max);
     printf("min - %d\n", min);
-    printf("number - %d\n", ind);
 }
