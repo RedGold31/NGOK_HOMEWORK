e5791dcb67b2728caad38f7d5226090f967d6b1f
RedGold31 <116094118+RedGold31@users.noreply.github.com>
2022-10-30 14:44:12 +0700

    Edd

--- massiv/main.c
+++ massiv/main.c
@@ -2,31 +2,32 @@
 #define SIZE 10
 
 // функция ввода элементов массива
-void set_mass(int a[], size); // заменить на указатель a[]
+void set_mass(int *a, int size); // заменить на указатель a[]
 // функция вывода элементов массива
-void print_mass(int a[], size);
+void print_mass(int a[], int size);
 // функция поиска максимального элемента в массиве
-int search_max(int a[], size);
+int search_max(int a[], int size);
 // функция поиска минимального элемента в массиве
-int search_min(int a[], size);
+int search_min(int a[], int size);
 // функция поиска индекса элемента в массиве по значению number
 int search_index(int a[], int size, int number);
 // меняет элементы местами (переворачивает массив) 
-int reserve(int a[], size); // заменить на указатель a[]
+int reserve(int *a, int size); // заменить на указатель a[]
 
 // добавить аргументу size в функции
 // разбить на файлы (main.c, array_func.c array_func.h)
 // добавить Makefile
 
-void set_mass(int a[])
+void set_mass(int *a, int size)
 {
+    
     for (int i = 0; i < SIZE; ++i) {
         scanf("%d", &a[i]);
         setbuf(stdin, NULL);
     }
 }
 
-void print_mass(int a[])
+void print_mass(int a[], int size)
 
 {
     for (int i = 0; i < SIZE; ++i) {
@@ -35,7 +36,7 @@ void print_mass(int a[])
     printf("\n");
 }
 
-int search_max(int a[])
+int search_max(int a[], int size)
 {
     int max = a[0];
     for (unsigned int i = 1; i < SIZE; ++i) {
@@ -50,28 +51,27 @@ int search_index(int a[], int size, int number)
 {
     // зачем 10?
    // удOли 
-    number = 10;
+    number = 0;
     int index = 0;
 
     for (int i = 0; i < number; ++i) {
-        // сканф не нужен, мы тут ничего не вводим, только ищем
-//         scanf("%d\n", &a[i]);
-//         printf("%d\n", a[i] - number);
         if(a[i] == number){
             index = i;
             break;
         }
+
     }
+
 
     return index;
 }
 
-int search_min(int a[])
+int search_min(int a[], int size)
 {
     // дописать как max
     int min = a[0];
 
-    for (int i = 0; i < SIZE; ++i) {
+    for (unsigned int i = 1; i < SIZE; ++i) {
         if (min > a[i]) {
             min = a[i];
         }
@@ -79,7 +79,7 @@ int search_min(int a[])
     return min;
 }
 
-int reserve(int a[])
+int reserve(int *a, int size)
 {
     // возвращает обратный массив
     for (int i = 0; i < SIZE; ++i) {
@@ -92,12 +92,14 @@ int main()
 {
 
     int a[SIZE] = {0};
-    set_mass(a);
-    print_mass(a);
-    search_max(a);
-    int res = reserve(a);
-    int max = search_max(a);
-    int min = search_min(a);
+    int number = 0;
+    set_mass(a, SIZE);
+    print_mass(a, SIZE);
+    reserve(a, SIZE);
+    int ind = search_index(a, SIZE, number);
+    int max = search_max (a, SIZE);
+    int min = search_min (a, SIZE);
     printf("max - %d\n", max);
     printf("min - %d\n", min);
+    printf("ind - %d\n", ind);
 }
