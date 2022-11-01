d8f86696d7107753046263e6f67438c115a851fd
RedGold31 <116094118+RedGold31@users.noreply.github.com>
2022-11-02 00:14:19 +0700

    FINAL

--- massiv/main.c
+++ massiv/main.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 #define SIZE 10
-#define FIND 7
+#define FIND 9
 
 int main()
 {
 
     int array[SIZE] = {0};
-    // int number = 0;
     set_mass(array, SIZE);
-    // print_mass(array, SIZE);
-    bubble_sort(array, SIZE);
-    // print_mass(array, SIZE);
-    // reserve(array, SIZE);
     print_mass(array, SIZE);
+    bubble_sort(array, SIZE);
     int a = binary_search(array, SIZE, FIND);
     int b = search_index(array, SIZE, FIND);
     printf("a = %d b = %d res = %s\n", a, b, a == b ? "true" : "false");
-    // int max = search_max(array, SIZE);
-    // int min = search_min(array, SIZE);
+    reserve(array, SIZE);
+    print_mass(array, SIZE);
+    int max = search_max(array, SIZE);
+    int min = search_min(array, SIZE);
+    printf("max - %d\n", max);
+    printf("min - %d\n", min);
 }
