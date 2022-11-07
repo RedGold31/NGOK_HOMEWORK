9134603cf2513bd4b2d16b77c6bcaacd06c7e994
RedGold31 <max10050031@gmail.com>
2022-11-07 18:52:04 +0700

    change

--- matrix/func.h
+++ matrix/func.h
@@ -7,11 +7,18 @@ typedef struct {
     int cols;
 } matrix_t;
 
-matrix_t create_matrix(const unsigned int rows, const unsigned int cols);
+matrix_t create_matrix(const int rows, const int cols);
 void print_matrix(matrix_t *matrix);
 void free_matrix(matrix_t *matrix);
 void fill_matrix(matrix_t *matrix);
 int eq_size(matrix_t *neo, matrix_t *Morfius);
 int eq_matrix(matrix_t *neo, matrix_t *Morfius);
+matrix_t sum_matrix(matrix_t *A, matrix_t *B);
+matrix_t sub_matrix(matrix_t *A, matrix_t *B);
+matrix_t mult_number(matrix_t *A, double num);
+matrix_t mult_matrix(matrix_t *A, matrix_t *B);
+matrix_t tran_matrix(matrix_t *A);
+matrix_t minr_matrix(matrix_t *A, int ip, int jp);
+double det_matrix(matrix_t *A);
 
 #endif // _FUNC_H_
